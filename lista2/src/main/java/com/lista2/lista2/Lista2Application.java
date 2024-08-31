66d97d734f8f98abac04f49615e61406df92097d
JohnnyMendesC <johnnymendescarmo@gmail.com>
2024-08-31 15:05:40 -0300

    Resolução do Exercício 1 e Otimização do Menu

--- lista2/src/main/java/com/lista2/lista2/Lista2Application.java
+++ lista2/src/main/java/com/lista2/lista2/Lista2Application.java
@@ -1,5 +1,7 @@
 package com.lista2.lista2;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,28 @@ public class Lista2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lista2Application.class, args);
+
+		Scanner leia = new Scanner(System.in);
+		int opcao;
+		System.out.println("\n\nBoas vindas a Lista de Exercicios 2 do Professor Arthur - SerraTec 2024-2");
+		System.out.print("Escolha um dos 8 exercícios digitando de 1 a 8 ou digite 0 para sair: ");
+		
+		opcao = leia.nextInt();		
+		switch(opcao){
+			case 1:
+			System.out.println("EXERCÍCIO 1:");
+			Exercicio01.resolucao();
+			break;
+
+			case 0:
+			System.out.println("Encerrando aplicação");
+			break;
+			default:
+			System.out.println("Digite uma opção entre 1 e 8");
+			
+		}
+		leia.close();
+		
 	}
 
 }
