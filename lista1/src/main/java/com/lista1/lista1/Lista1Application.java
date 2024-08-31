80ff7645ba7a4b8d6095661c9914009161c34009
JohnnyMendesC <johnnymendescarmo@gmail.com>
2024-08-30 22:09:15 -0300

    Criação do Menu Principal e Resolução do Exercício 1

---
+++ lista1/src/main/java/com/lista1/lista1/Lista1Application.java
@@ -0,0 +1,33 @@
+package com.lista1.lista1;
+
+import java.lang.runtime.SwitchBootstraps;
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lista1Application.class, args);
+
+		int opcao;
+		System.out.println("Boas vindas a Lista de Exercicios 1 do Professor Arthur - SerraTec 2024-2");
+		System.out.println("Escolha um dos exerecícios:");
+		Scanner leia = new Scanner(System.in);
+		opcao = leia.nextInt();
+
+		switch(opcao){
+			case 1:
+			System.out.println("EXERCÍCIO 1:");
+			Exercicio01.resolucao();
+			break;
+			
+		
+	}
+
+		leia.close();
+	}
+
+}
