fd39fa4846776cbc8b0f112db938e4c0a269184d
JohnnyMendesC <johnnymendescarmo@gmail.com>
2024-09-03 18:54:42 -0300

    Resolução do Exercício01 da lista3

--- listas/src/main/java/lista3/Exercicio01.java
+++ listas/src/main/java/lista3/Exercicio01.java
@@ -1,5 +1,7 @@
 package lista3;
 
+import java.util.Scanner;
+
 public class Exercicio01 {
     public static void resolucao(){
         System.out.println(
@@ -8,6 +10,31 @@ public class Exercicio01 {
             +"digitados. O programa deve receber somente números entre 1 e 59, do contrário \n"
             +"ele deve retornar um erro e solicitar que o usuário digite um número válido\n"
         );
+        Scanner leia = new Scanner(System.in);
 
+        int segundos;
+        System.out.print("\nDe 1 a 59, digite quantos segundos você deseja cronometrar: ");
+        segundos = leia.nextInt();
+
+        while(segundos<1 || segundos>59){            
+                System.out.print("\nDe 1 a 59, digite quantos segundos você deseja cronometrar: ");
+                segundos = leia.nextInt();            
+        }
+
+        
+        if (segundos==1){System.out.println("Iniciando cronômetro com "+segundos+" segundo...");}
+        else {System.out.println("Iniciando cronômetro com "+segundos+" segundos...");}
+        
+        for(int timer = 0; timer <= segundos; timer++){
+            
+            try {
+                Thread.sleep(1000); // Aguarda 1 segundo                
+                System.out.println("Segundos: " + timer);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } //metodo try catch, o catch é caso de erro para ele nao quebrar o codigo, o try é o código executado
+        }
+        System.out.println("Timer finalizado!");
+        leia.close();
     }
 }
--- listas/src/main/java/lista3/Lista3Application.java
+++ listas/src/main/java/lista3/Lista3Application.java
@@ -5,26 +5,35 @@ import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import lista3.Lista3Application;
-
 @SpringBootApplication
 public class Lista3Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Lista3Application.class, args);
         
-        Scanner leia = new Scanner(System.in); 
+        Scanner leiaopcao = new Scanner(System.in); 
 
         int opcao;
+        System.out.print(
+            "\n\n==========================================================================\n"
+            +"Boas vindas a Lista de Exercicios 3 do Professor Arthur - SerraTec 2024-2\n"
+            +"Escolha um dos 7 exercícios digitando de 1 a 7 ou digite 0 para sair: "
+            );
 
-        opcao = leia.nextInt();
-        leia.close();
-        
+        opcao = leiaopcao.nextInt();        
         switch(opcao){
         case 1:
         Exercicio01.resolucao();
         break;
 
+        case 0:
+        System.out.println("Encerrando aplicação");
+        break;
+        default:
+        System.out.println("Digite uma opção entre 1 e 7");
+
         }
+        leiaopcao.close();
     }
 
 }
