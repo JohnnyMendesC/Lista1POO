c3a1f2aaaf48d955287233a34b6fbda9b726c2c7
JohnnyMendesC <johnnymendescarmo@gmail.com>
2024-09-04 19:00:00 -0300

    Resolução do Exercício01 da lista4

--- listas/src/main/java/lista4/Exercicio01.java
+++ listas/src/main/java/lista4/Exercicio01.java
@@ -2,7 +2,26 @@ package lista4;
 
 public class Exercicio01 {
     public static void resolucao(){
-        
+        System.out.println("==========================================================================");
+        System.out.println(
+            "1) Você é gerente de um supermercado e sabe que os valores das"
+            +"\nvendas do dia são gravados em um vetor. Digite um código que faça o"
+            +"\nfechamento)(soma) de vendas do dia de maneira automatizada.\n"
+        );
+
+        double[] vendas = {10.00, 5.50, 16.00, 1.00, 2.50, 1.00};
+        double soma=0;
+        double valor;
+
+        for(int i=0; i<6;i++){
+            System.out.println((i+1)+"º venda | R$"+vendas[i]);
+            valor = vendas[i];
+            soma += valor;
+            
+        }
+        System.out.println("_________________________");
+        System.out.println("Fechamento do dia R$"+soma);
+        System.out.println("_________________________");
     }
 
 }
